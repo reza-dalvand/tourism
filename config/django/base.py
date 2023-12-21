7d94da48d7c0b4003b1ca8be35e73157141f078f
Reza <rdalvand@yahoo.com>
2023-12-21 09:16:45 +0330

    config rest in project

--- config/django/base.py
+++ config/django/base.py
@@ -16,7 +16,9 @@ DJANGO_APPS = [
     "django.contrib.staticfiles",
 ]
 
-THIRD_PARTY_APPS = []
+THIRD_PARTY_APPS = [
+                            'rest_framework',
+]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS
 

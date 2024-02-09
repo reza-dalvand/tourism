9bdd70cdf18588555e64972be7f43ea4ac4b72d5
Reza <rdalvand@yahoo.com>
2024-02-09 08:33:25 +0330

    write test for hotel api

--- apps/tests/hotels/hotel.py
+++ apps/tests/hotels/hotel.py
@@ -3,6 +3,7 @@ from model_bakery import baker
 from rest_framework import status
 from setup import SetUp
 
+from apps.hotels.models import Hotel
 from apps.users.models import User
 
 
@@ -11,18 +12,19 @@ class TestHotel(SetUp):
     def setup_data(self):
         print("setup...")
         self.user = baker.make(User, is_hotel_owner=True)
+        self.hotel = baker.make(Hotel, hotel_name="test")
         self.api_client.force_authenticate(self.user)
         yield "setup_data"
         print("tear down...")
 
     def test_hotel(self, setup_data):
         data = {
-            "hotel_name": "test",
+            "hotel_name": "example",
             "phone": "02112345678",
             "email": "example@example.com",
             "number_of_rooms": 5,
-            "short_des": "test",
-            "description": "test",
+            "short_des": "example",
+            "description": "example",
             "owner": 1,
         }
         response = self.api_client.post(self.hotel_url, data=data)
@@ -37,3 +39,11 @@ class TestHotel(SetUp):
         self.user.save()
         response = self.api_client.post(self.hotel_url, data={})
         assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_remove_hotel(self, setup_data):
+        response = self.api_client.delete(f"{self.hotel_url}{self.hotel.hotel_name}/")
+        assert response.status_code == status.HTTP_204_NO_CONTENT
+
+    def test_update_hotel(self, setup_data):
+        response = self.api_client.patch(f"{self.hotel_url}{self.hotel.hotel_name}/", data={"email": "test@test.com"})
+        assert response.status_code == status.HTTP_200_OK

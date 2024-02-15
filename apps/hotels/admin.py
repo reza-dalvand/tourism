8da71cc2f8a0a264312bddff50695dbddaf81637
Reza <rdalvand@yahoo.com>
2024-02-15 08:37:21 +0330

    added models to admin apps

--- apps/hotels/admin.py
+++ apps/hotels/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from apps.hotels.models import Hotel, HotelAddress, Reservation, Room
+
 # Register your models here.
+admin.site.register(HotelAddress)
+admin.site.register(Hotel)
+admin.site.register(Reservation)
+admin.site.register(Room)
--- apps/tours/admin.py
+++ apps/tours/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from apps.tours.models import Tour, TourismCompany, TourismCompanyAddress, TourReservation
+
 # Register your models here.
+admin.site.register(TourismCompanyAddress)
+admin.site.register(TourismCompany)
+admin.site.register(TourReservation)
+admin.site.register(Tour)
